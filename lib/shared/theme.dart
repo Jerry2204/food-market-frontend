f147299c3710e96ecb9af9c3637db6d1a3a5a740
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2024-08-26 17:28:40 +0700

    Initialized commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,9 @@
+part of 'shared.dart';
+
+// Color
+Color mainColor = Color(0xffFFC700);
+Color greyColor = Color(0xff8D92A3);
+
+TextStyle greyFontTextStyle = GoogleFonts.poppins().copyWith(color: greyColor);
+
+const double defaultMargin = 24;
